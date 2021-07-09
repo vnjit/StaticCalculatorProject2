55d0f496e0aef2a9b1928eb7deb93d233b8cafb2
vidhipatel <vkp8.njit.edu>
2021-07-09 15:13:25 -0400

    Task: Update static file

---
+++ src/StaticProperties/StaticVariable.py
@@ -0,0 +1,11 @@
+# Assign Static variable to Static Text
+class StaticVariable:
+    unitTestAddition = "./src/Tests/Data/Addition.csv"
+    unitTestSubtraction = "./src/Tests/Data/Subtraction.csv"
+    unitTestMultiplication = "./src/Tests/Data/Multiplication.csv"
+    unitTestDivision = "./src/Tests/Data/Division.csv"
+    unitTestSquare = "./src/Tests/Data/Square.csv"
+    unitTestSquareRoot = "./src/Tests/Data/SquareRoot.csv"
+    value1 = "Value 1"
+    value2 = "Value 2"
+    result = "Result"
