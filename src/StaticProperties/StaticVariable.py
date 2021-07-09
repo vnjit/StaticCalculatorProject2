bd5fd6ff3ed93b8e3041e1c1c0bed9f4f90205a3
vidhipatel <vkp8.njit.edu>
2021-07-09 15:19:04 -0400

    Task: Update addition Method

--- src/StaticProperties/StaticVariable.py
+++ src/StaticProperties/StaticVariable.py
@@ -1,11 +1,11 @@
 # Assign Static variable to Static Text
 class StaticVariable:
-    unitTestAddition = "./src/Tests/Data/Addition.csv"
-    unitTestSubtraction = "./src/Tests/Data/Subtraction.csv"
-    unitTestMultiplication = "./src/Tests/Data/Multiplication.csv"
-    unitTestDivision = "./src/Tests/Data/Division.csv"
-    unitTestSquare = "./src/Tests/Data/Square.csv"
-    unitTestSquareRoot = "./src/Tests/Data/SquareRoot.csv"
+    TestAddition = "./src/Tests/Data/Addition.csv"
+    TestSubtraction = "./src/Tests/Data/Subtraction.csv"
+    TestMultiplication = "./src/Tests/Data/Multiplication.csv"
+    TestDivision = "./src/Tests/Data/Division.csv"
+    TestSquare = "./src/Tests/Data/Square.csv"
+    TestSquareRoot = "./src/Tests/Data/SquareRoot.csv"
     value1 = "Value 1"
     value2 = "Value 2"
     result = "Result"
--- src/Tests/test_CalculatorTest.py
+++ src/Tests/test_CalculatorTest.py
@@ -1,23 +1,24 @@
 import unittest
 from src.Calculator.Calculator import Calculator
 from src.CsvReader.CsvReader import CsvReader
+from src.StaticProperties.StaticVariable import StaticVariable
 
 class MyTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.mainCalculator = MainCalculator()
+        self.Calculator = Calculator()
 
     def test_instantiate_calculator(self):
-        self.assertIsInstance(self.mainCalculator, MainCalculator)
+        self.assertIsInstance(self.Calculator, Calculator)
 
         # Unit Test for Addition
 
     def test_addition(self):
-        test_add_data = CSVReader(StaticVariable.unitTestAddition).data
+        test_add_data = CsvReader(StaticVariable.TestAddition).data
         for row in test_add_data:
-            self.assertEqual(self.mainCalculator.add(row[StaticVariable.value1], row[StaticVariable.value2]),
+            self.assertEqual(self.Calculator.add(row[StaticVariable.value1], row[StaticVariable.value2]),
                              int(row[StaticVariable.result]))
-            self.assertEqual(self.mainCalculator.result, int(row[StaticVariable.result]))
+            self.assertEqual(self.Calculator.result, int(row[StaticVariable.result]))
 
 
 if __name__ == '__main__':
