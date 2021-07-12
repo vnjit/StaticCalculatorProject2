60db2efbf2adee315442a15e98112edbcede2813
vidhipatel <vkp8.njit.edu>
2021-07-12 11:47:43 -0400

    Task: Updated statistics and Mean files

--- src/Statistics/Mean.py
+++ src/Statistics/Mean.py
@@ -2,7 +2,7 @@ from src.Calculator.Addition import addition
 from src.Calculator.Division import division
 
 
-def mean(data):
+def get_mean(data):
     num_values = len(data)
     total = 0
     for num in data:
--- src/Statistics/Statistics.py
+++ src/Statistics/Statistics.py
@@ -1,9 +1,9 @@
 from src.Calculator.Calculator import Calculator
-from src.Statistics.Mean import mean
+from src.Statistics.Mean import get_mean
 
 
 class Statistics(Calculator):
 
     def mean(self, data):
-        self.result = mean(data)
+        self.result = get_mean(data)
         return self.result
