6f0647a698121b09877689315827a8224280ec94
vidhipatel <vkp8.njit.edu>
2021-07-12 17:30:37 -0400

    Task:  Upadated Files

--- src/Statistics/Mean.py
+++ src/Statistics/Mean.py
@@ -3,13 +3,9 @@ from src.Calculator.Division import division
 
 
 def get_mean(data):
-    try:
-        num_values = len(data)
-        total = 0
-        for num in data:
-            total = addition(total, num)
-        return division(num_values, total)
-    except ZeroDivisionError:
-        print("Error: Can't Divide by 0")
-    except ValueError:
-        print("ERROR!  That is an empty array.  Try again")
+    num_values = len(data)
+    total = 0
+    for num in data:
+        total = addition(total, num)
+
+    return division(num_values, total)
