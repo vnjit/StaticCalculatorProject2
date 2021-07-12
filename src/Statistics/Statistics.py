969cf513cb6d24bb9feaa4b99409131bfaa8c56c
vidhipatel <vkp8.njit.edu>
2021-07-12 11:51:25 -0400

    Task: Updated statistics and Mean files

--- src/Statistics/Statistics.py
+++ src/Statistics/Statistics.py
@@ -3,6 +3,10 @@ from src.Statistics.Mean import get_mean
 
 
 class Statistics(Calculator):
+    data = []
+
+    def __init__(self):
+        super().__init__()
 
     def mean(self, data):
         self.result = get_mean(data)
