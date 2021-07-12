9b1dab9002aed75915d3fb61758430e3ae9b2017
vidhipatel <vkp8.njit.edu>
2021-07-12 11:59:31 -0400

    Task:  Created Median file and method

---
+++ src/Statistics/Median.py
@@ -0,0 +1,18 @@
+from src.Calculator.Addition import addition
+from src.Calculator.Division import division
+
+
+def get_mean(data):
+    num_values = len(data)
+    if num_values % 2 == 0:
+        value = int(division(2, num_values))
+        a = data[value]
+        value = value - 1
+        b = data[value]
+        c = addition(b, a)
+        d = division(2, c)
+        return d
+    else:
+        value = int(division(2, num_values))
+        e = data[value]
+        return e
--- src/Statistics/Statistics.py
+++ src/Statistics/Statistics.py
@@ -1,5 +1,6 @@
 from src.Calculator.Calculator import Calculator
 from src.Statistics.Mean import get_mean
+from src.Statistics.Mean import get_median
 
 
 class Statistics(Calculator):
@@ -10,4 +11,8 @@ class Statistics(Calculator):
 
     def mean(self, data):
         self.result = get_mean(data)
+        return self.result
+
+    def median(self, data):
+        self.result = get_median(data)
         return self.result
