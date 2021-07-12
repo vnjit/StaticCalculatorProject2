00438da601aae7fde2442763184b5affde80c1cb
vidhipatel <vkp8.njit.edu>
2021-07-12 12:54:14 -0400

    Task:  Created Mode file and method

--- src/Statistics/Statistics.py
+++ src/Statistics/Statistics.py
@@ -1,6 +1,7 @@
 from src.Calculator.Calculator import Calculator
 from src.Statistics.Mean import get_mean
 from src.Statistics.Median import get_median
+from src.Statistics.Mode import get_mode
 
 
 class Statistics(Calculator):
@@ -16,3 +17,8 @@ class Statistics(Calculator):
     def median(self, data):
         self.result = get_median(data)
         return self.result
+
+    def mode(self, data):
+        self.result = get_mode(data)
+        return self.result
+
