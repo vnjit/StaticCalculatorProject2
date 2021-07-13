f183b6e2709146f661f9a5e43e8dcb7dd4a14ad7
vidhipatel <vkp8.njit.edu>
2021-07-13 13:25:27 -0400

    Task:  Updated File

--- src/Statistics/Median.py
+++ src/Statistics/Median.py
@@ -1,22 +1,22 @@
 from src.Calculator.Addition import addition
-from src.Calculator.Subtraction import subtraction
 from src.Calculator.Division import division
 
 
 def get_median(data):
-    try:
-        total = len(data)
-        data.sort()
-        if total % 2 == 0:
-            median1 = data[int(division(2, total))]
-            median2 = data[int(subtraction(division(2, total)), 1)]
-            result = division(2, addition(median1, median2))
-        else:
-            result = data[int(division(2, total))]
+    num_values = len(data)
+    if num_values % 2 == 0:
+        value = int(division(2, num_values))
+        a = data[value]
+        value = value - 1
+        b = data[value]
+        c = addition(b, a)
+        d = division(2, c)
+        return d
+    else:
+        value = int(division(2, num_values))
+        e = data[value]
+        return e
 
-            return result
-    except ValueError:
-            print("ERROR!  Data is Empty.  Try again.")
 
 
 
